53160b808508b098ddae8c03047d1cb79292a0e6
srijana <b180008@nitsikkim.ac.in>
2025-02-11 22:44:06 +0530

    cros added again

--- ai_backend/settings.py
+++ ai_backend/settings.py
@@ -148,13 +148,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ai-frontend-k1zu.onrender.com']
 CORS_ALLOWED_ORIGINS = [
    "https://ai-frontend-k1zu.onrender.com",
    "http://localhost:5173",
 
 ]
-CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOW_CREDENTIALS = True
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
