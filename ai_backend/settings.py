93bafd108d227f429135904e63a64669be63df48
srijana <b180008@nitsikkim.ac.in>
2025-02-12 12:59:09 +0530

    https added

--- ai_backend/settings.py
+++ ai_backend/settings.py
@@ -148,7 +148,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-ALLOWED_HOSTS = ['ai-frontend-k1zu.onrender.com']
+ALLOWED_HOSTS = ['https://ai-frontend-k1zu.onrender.com']
 CORS_ALLOWED_ORIGINS = [
    "https://ai-frontend-k1zu.onrender.com",
    "http://localhost:5173",
